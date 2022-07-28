0d3ec763cf74e426578d01a21cc348f802a3e6a1
thomaswilliams23 <62917774+thomaswilliams23@users.noreply.github.com>
2022-07-28 13:21:27 +1000

    Delete mesh_params.m

--- helpers/mesh_params.m
+++
@@ -1,9 +0,0 @@
-
-coarse_mesh = (pde_dx>1);
-
-mesh_width = round(cells_wide/pde_dx)+2;
-mesh_length = round(cells_long/pde_dx)+2;
-total_mesh_nodes = mesh_width*mesh_length;
-
-courant = virus_diff*pde_dt/pde_dx^2;
-assert(courant<=0.25)
